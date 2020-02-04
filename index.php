9d4ebd96958440da863633b57530f30046c8b14d
Pedro Botega <botegapedro@gmail.com>
2020-02-04 07:49:29 -0300

    criado e configurado 'index.php'

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Localhost</title>
+    <style>
+        #caixa{
+            height: 400px;
+            width: 600px;
+            margin: auto;
+            background-color:#4d4d4d;
+            border-radius: 4px;
+            box-shadow: 15px 15px black;
+            transition-duration: .3s;
+        }
+        #conteudo{
+            height: 400px;
+            width: 600px;
+            padding: 10px;
+        }
+        body{
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            color: white;
+        }
+        #caixa:hover {
+            box-shadow: 25px 25px black;
+            border-radius: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div id="total">
+        <div id="caixa">
+            <div id="conteudo">
+                <h1>
+                    Início da pasta 'LocalHost'
+                </h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
