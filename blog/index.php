0ea69ec077963d201c5daa5df12d169dd9afc2fd
Pedro Botega <botegapedro@gmail.com>
2020-02-06 12:21:16 -0300

    excluído arquivo 'nav'

--- blog/index.php
+++ blog/index.php
@@ -1,4 +1,36 @@
-<?php
-    include_once('C:\xampp\htdocs\nav.php');
-    echo basename(__DIR__);
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        <?php echo $pasta_local ?>
+    </title>
+    <link rel="shortcut icon" href="https://i.imgur.com/O7CRS3b.png">
+    <link rel="stylesheet" type="text/css" href="http://localhost/styles.css"/>
+</head>
+    <body>
+        <div id="total">
+            <div id="caixa">
+                <div id="conteudo">
+                
+                    <h1>
+                        Listagem para pasta '<?php echo basename(__DIR__); ?>'
+                    </h1>
+                    <h3 class="subtitulo">
+                        Listagem das pastas:
+                    </h3>
+                    <div id="listagem">
+                        <?php
+                            $dir = scandir(dirname(__FILE__));
+                            foreach ($dir as $d){
+                                echo '<ul>'.'<li>'.'<a href="/'.$d.'">'.$d.'<br>'.'</a>'.'</li>'.'</ul>';
+                            }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
