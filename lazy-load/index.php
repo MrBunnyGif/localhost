bfba86939dec2ba99248afe2b08c25077b54e680
Pedro Botega <botegapedro@gmail.com>
2020-02-13 10:00:29 -0300

    criada borda para lazy load

---
+++ lazy-load/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lazy Load</title>
+    <style>
+        .teste{
+            border: 2px dashed black;
+            height: 900px;
+        }
+    </style>
+</head>
+<body>
+    <article class="teste">
+
+    </article>
+</body>
+</html>
