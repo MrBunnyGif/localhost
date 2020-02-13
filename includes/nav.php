ec7b40f2c59e84bea36e2ddd3ddac2670993831a
Pedro Botega <botegapedro@gmail.com>
2020-02-13 09:57:44 -0300

    criada pasta 'lazyload'

---
+++ includes/nav.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        <?php
+            $titulo ;
+        ?>
+    </title>
+    <link rel="shortcut icon" href="https://i.imgur.com/O7CRS3b.png">
+    <link rel="stylesheet" type="text/css" href="http://localhost/styles.css"/>
+</head>
+    <body>
+        <div id="total">
+            <div id="caixa">
+                <div id="conteudo">
+                
+                    <h1>
+                        Listagem para pasta '<?php echo $titulo; ?>'
+                    </h1>
+                    <h3 class="subtitulo">
+                        Listagem das pastas:
+                    </h3>
+                    <div id="listagem">
+                    <?php
+                        $dir = scandir($list);
+                        foreach ($dir as $d){
+                            echo '<ul>'.'<li>'.'<a href="'.$d.'">'.$d.'<br>'.'</a>'.'</li>'.'</ul>';
+                        }
+                    ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
