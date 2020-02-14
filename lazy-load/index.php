ea9de8882df58f94349b9148384ba1ff5f51a9ad
Pedro Botega <botegapedro@gmail.com>
2020-02-14 09:05:15 -0300

    alteradas alturas do nav

--- lazy-load/index.php
+++ lazy-load/index.php
@@ -5,16 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lazy Load</title>
     <style>
-        .teste{
+        article{
             border: 2px dashed black;
-            height: 900px;
+            height: 600vh;
+        }
+        .lazy-img{
+            margin: auto;
+            width: 500px;
+            height: 700px;
+            border: 2px solid gray;
+            margin-top: 150vh;
         }
     </style>
     <script src="jquery-3.4.1.min.js"></script>
+    <script src="script.js"></script>
 </head>
 <body>
     <article class="teste">
-
+        <div class="lazy-img">
+            <img src="" data-url="https://i.imgur.com/vVf35xV.jpg" width="495px" alt="">
+        </div>
     </article>
 </body>
 </html>
--- lazy-load/script.js
+++ lazy-load/script.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
     $(document).on("scroll", function(){
-        console.log("Scroll feito");
+        var image = $(".lazy-img img");
+        url = image.data("url");
+        image.attr("src", url);
+        if (isOnScreen ($(".lazy-img"))){
+            $("article").css("background-color", "red");
+        }
+        // else{
+        //     $("article").css("background-color","");
+        // }
     });
 });
+
+function isOnScreen(element){
+    var win = $(window);
+    
+    var elementTop = element.offset().top;
+    var elementBottom = elementTop + element.height();
+
+    var screenTop = win.scrollTop();
+    var screenBottom = screenTop + win.height();
+    
+
+    return elementBottom > screenTop && elementTop < screenBottom;
+}
+
+// var loadImages;
+// 
+// $(document).ready(function(){
+    // $(document).on("scroll", function(){
+        // loadImages();
+    // });
+// 
+    // (loadImages = function(){
+        // $.each ($(".lazy.img"), function(){
+            // var block = $(this);
+            // var image = block.find("img");
+// 
+            // if(isOnScreen(block)){
+                // var url = img.data("url");
+                // if(image.attr("src")!=url){
+                    // image.attr("src", url);
+                // };
+            // }
+        // });
+    // })();
+    // $(".lazy-img").on('load',function(){
+        // $(this).addClass("loaded");
+    // });
+// });
