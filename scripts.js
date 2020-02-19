4099da638d8bbf6c533c52890ef14d20a464d043
Pedro Botega <botegapedro@gmail.com>
2020-02-19 19:34:15 -0300

    esc funcionando corretamente

--- scripts.js
+++ scripts.js
@@ -5,24 +5,45 @@ var divFile = document.getElementById('cArquivo');
 var divFolder = document.getElementById('cPasta');
 var fundo = document.getElementById('fundo');
 
-// Pressionada tecla esc
-document.addEventListener('keydown', function(event) {
-    const key = event.key; // Or const {key} = event; in ES6+
-    if (key === "Escape") {
-        alert("Está funcionando");
-    }
-});
-
 btnFile.addEventListener("click", function(){
 	divFile.classList.toggle('-active');
-	fundo.classList.toggle('-active');
-	if (aparecer.classList.contains('-active')){
-		fundo.addEventListener("click", function(){
-			divFile.classList.toggle('-active');
+	divFile.style.display = "block";
+	fundo.style.display = "block";
+	if ((divFile.classList.contains('-active'))){
+		document.addEventListener('keydown', function() {
+			var key = event.key;
+			if (key === "Escape") {
+				divFile.classList.toggle('-active');
+				divFile.style.display = "none";
+				fundo.style.display = "none";
+			}
 		});
-	};
+	}
 });
-
-// if (aparecer.classList.contains('-active')){
-// 	fundo.classList.toggle('-active');
-// };
+btnFolder.addEventListener("click", function(){
+	divFolder.classList.toggle('-active');
+	divFolder.style.display = "block";
+	fundo.style.display = "block";
+	if ((divFolder.classList.contains('-active'))){
+		document.addEventListener('keydown', function() {
+			var key = event.key;
+			if (key === "Escape") {
+				divFolder.classList.toggle('-active');
+				divFolder.style.display = "none";
+				fundo.style.display = "none";
+			}
+		});
+	}
+	
+});
+fundo.addEventListener("click", function(){
+	if (divFile.classList.contains('-active')){
+		divFile.classList.toggle('-active');
+		divFile.style.display = "none";
+	};
+	if (divFolder.classList.contains('-active')){
+		divFolder.classList.toggle('-active');
+		divFolder.style.display = "none";
+	};
+	fundo.style.display = "none";
+});
