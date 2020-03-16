debcfb8733d5b97d662d0c9beb83dd37d2bc471e
Pedro Botega <botegapedro@gmail.com>
2020-03-16 13:40:35 -0300

    terminada primeira secção do curso

--- udemy/HeadersAnimados/ecoworld/ecoworld/scripts/script.js
+++ udemy/HeadersAnimados/ecoworld/ecoworld/scripts/script.js
@@ -1,20 +1,24 @@
 let logo = document.querySelectorAll('.logo')[0];
 window.addEventListener('scroll', toogleNavBar, false);
+const navegacao = document.querySelectorAll('.nav-bar')[0];
 function toogleNavBar() {
-    const navegacao = document.querySelectorAll('.nav-bar')[0];   
-    if (window.pageYOffset >= logo.offsetHeight && navegacao.classList.contains('abs-pos')) {
-        navegacao.classList.remove('abs-pos');
-        navegacao.classList.add('fix-pos');
-    }
-    else if (window.pageYOffset <= logo.offsetHeight && navegacao.classList.contains('fix-pos')) {
-        navegacao.classList.remove('fix-pos');
-        navegacao.classList.add('abs-pos');
-    }
+	if (window.pageYOffset >= logo.offsetHeight && navegacao.classList.contains('abs-pos')) {
+		navegacao.classList.remove('abs-pos');
+		navegacao.classList.add('fix-pos');
+	}
+	else if (window.pageYOffset <= logo.offsetHeight && navegacao.classList.contains('fix-pos')) {
+		navegacao.classList.remove('fix-pos');
+		navegacao.classList.add('abs-pos');
+	}
 }
-
-const extLink = document.querySelectorAll('.ext-link')[0];
-extLink.addEventListener('click', openLink, false);
-
-function openLink(){
-    window.open('https://www.blogger.com/about/', '_blank');
-}
+const intLink = document.querySelectorAll('.int-link a')
+intLink.forEach(function (cur) {
+	cur.addEventListener('click', function (evt) {
+		evt.preventDefault();
+		window.scrollTo({
+			top: document.querySelector(cur.getAttribute('href')).offsetTop - navegacao.offsetHeight,
+			left: 0,
+			behavior: 'smooth'
+		})
+	}, false)
+})
